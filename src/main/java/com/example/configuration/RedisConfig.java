466c63aabd21464f00e36ef0e95061760b6b91be
Salman Kazmi <Salman.Kazmi@freschelegacy.com>
2018-09-03 17:10:20 +0530

    initial commit

---
+++ src/main/java/com/example/configuration/RedisConfig.java
@@ -0,0 +1,6 @@
+package com.example.configuration;
+
+
+public class RedisConfig {
+
+}
---
+++ src/main/java/com/example/demo/Abc.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import java.util.LinkedList;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Abc {
+	public void check() {
+		LinkedList<String> proverbs = new LinkedList<>();
+	    LinkedList<Double> numbers = new LinkedList<>();
+	    
+	    System.out.println("numbers class name " + numbers.getClass().getName());
+	    System.out.println("proverbs class name " + proverbs.getClass().getName());
+	    System.out.println("Compare Class objects: " +
+	                               numbers.getClass().equals(proverbs.getClass()));
+
+	}
+}
