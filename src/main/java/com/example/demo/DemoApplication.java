dcbce989e7846b5278be6db779f2fb1927c821b1
salmankazmi <slmnkzm@gmail.com>
2018-09-05 19:38:52 +0530

    Changes for H2 DB integration

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,25 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.example.utils.CachePlayground;
-import com.example.utils.StackPlayground;
 
 @SpringBootApplication
+@ComponentScan("com.example")
 @EnableCaching
 public class DemoApplication {
-	
-	@Autowired
-	CachePlayground cp;
 	
 	private final Logger LOG = LoggerFactory.getLogger(getClass());
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		
-		StackPlayground sp = new StackPlayground(10);
-		sp.push(12);
-		sp.push(10);
-		System.out.println(sp.peek());
 	}
 }
