43e95cad4ffa14c8895c97d359c0f60ed83b5afc
Salman Kazmi <Salman.Kazmi@freschelegacy.com>
2018-09-11 19:58:12 +0530

    Code Formatting

--- src/main/java/com/example/constants/ResultCode.java
+++ src/main/java/com/example/constants/ResultCode.java
@@ -1,24 +1,29 @@
 package com.example.constants;
 
 public enum ResultCode {
-	
-	SUCCESS("Success", 0),ERROR("Error", 1);
-	
+
+	SUCCESS("Success", 0), ERROR("Error", 1);
+
 	private String message;
 	private Integer code;
+
 	ResultCode(String message, Integer code) {
 		this.message = message;
 		this.code = code;
 	}
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
+
 	public Integer getCode() {
 		return code;
 	}
+
 	public void setCode(Integer code) {
 		this.code = code;
 	}
